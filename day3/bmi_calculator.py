a7082d324e0addcdf31f3ab0009e41aae58e6838
jimmy-do <59305538+jimmy-do@users.noreply.github.com>
2023-07-13 18:24:02 -0500

    rebase leap yr commit

--- day3/bmi_calculator.py
+++ day3/bmi_calculator.py
@@ -1,7 +1,7 @@
 # Simple BMI Calculator
 # BMI = (weight) / (height^2)
 
-weight = int(input('How much do you weight in kg? '))
+weight = float(input('How much do you weight in kg? '))
 height = float(input('How tall are you in m?'))
 
 bmi = round(weight / (height ** 2))
@@ -11,10 +11,10 @@ bmi = round(weight / (height ** 2))
 if bmi < 18.5:
     print('Your bmi is ' + bmi + ', you are underweight.')
 elif bmi < 25:
-    print('You are normal weight.')
+    print(f'Your bmi is {bmi}, you are normal weight.')
 elif bmi < 30:
-    print('You are overweight.')
+    print(f'Your bmi is {bmi}, you are overweight.')
 elif bmi < 35:
-    print('You are obese.')
+    print(f'Your bmi is {bmi}, you are obese.')
 else:
-    print('You are clinically obese.')
+    print(f'Your bmi is {bmi}, you are clinically obese.')
