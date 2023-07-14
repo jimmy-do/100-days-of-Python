80926ad8b2a04dced28b599a916eed68060483de
jimmy-do <59305538+jimmy-do@users.noreply.github.com>
2023-07-13 19:43:16 -0500

    pizza order commit + updated roller coaster

--- day3/rollercoaster.py
+++ day3/rollercoaster.py
@@ -10,10 +10,22 @@ if height > 48:
     print('You can ride the ride!!!!')
     age = int(input('How old are you? '))
     if age < 12:
-        print('Pay $5')
+        photos = input('Do you want photos?')
+        if photos == 'yes':
+            print('Pay $8')
+        else:
+            print('Pay $5')
     elif age < 18:
-        print('Pay $7')
+        photos = input('Do you want photos?')
+        if photos == 'yes':
+            print('Pay $10')
+        else:
+            print('Pay $7')
     elif age >= 18:
-        print('Pay $12')
+        photos = input('Do you want photos?')
+        if photos == 'yes':
+            print('Pay $15')
+        else:
+            print('Pay $12')
 else:
     print('You cannot ride the ride little one.')
