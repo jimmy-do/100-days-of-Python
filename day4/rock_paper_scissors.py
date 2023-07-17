88d6d5b296c952ab42e930eede3662a07e97773e
jimmy-do <59305538+jimmy-do@users.noreply.github.com>
2023-07-16 22:25:19 -0500

    rock_paper_scissors - removing redundant code

--- day4/rock_paper_scissors.py
+++ day4/rock_paper_scissors.py
@@ -36,34 +36,35 @@ player_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2
 print(f'You chose: {rock_paper_scissors[player_choice]}')
 
 computer_choice = random.choice(rock_paper_scissors)
+print(f'Computer chose: {computer_choice}')
 
 if player_choice == 0:
     if computer_choice == rock:
-        print(f'Computer chose: {computer_choice}')
+        # print(f'Computer chose: {computer_choice}')
         print('Tie.')
     elif computer_choice == paper:
-        print(f'Computer chose: {paper}')
+        # print(f'Computer chose: {computer_choice}')
         print('You lose.')
     elif computer_choice == scissors:
-        print(f'Computer chose: {scissors}')
+        # print(f'Computer chose: {computer_choice}')
         print('You win!')
 elif player_choice == 1:
     if computer_choice == rock:
-        print(f'Computer chose: {computer_choice}')
+        # print(f'Computer chose: {computer_choice}')
         print('You win!')
     elif computer_choice == paper:
-        print(f'Computer chose: {paper}')
+        # print(f'Computer chose: {computer_choice}')
         print('Tie.')
     elif computer_choice == scissors:
-        print(f'Computer chose: {scissors}')
+        # print(f'Computer chose: {computer_choice}')
         print('You lose.')
 elif player_choice == 2:
     if computer_choice == rock:
-        print(f'Computer chose: {computer_choice}')
+        # print(f'Computer chose: {computer_choice}')
         print('You lose')
     elif computer_choice == paper:
-        print(f'Computer chose: {paper}')
+        # print(f'Computer chose: {computer_choice}')
         print('You win!')
     elif computer_choice == scissors:
-        print(f'Computer chose: {scissors}')
+        # print(f'Computer chose: {computer_choice}')
         print('Tie.')
