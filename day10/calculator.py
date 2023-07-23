9c3182e9445b2946d55c06dc0cdfb36fefa38854
jimmy-do <59305538+jimmy-do@users.noreply.github.com>
2023-07-22 21:48:48 -0500

    calculator.py

--- day10/calculator.py
+++ day10/calculator.py
@@ -1,3 +1,7 @@
+from art import logo
+print(logo)
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -6,12 +10,23 @@ def subtract(n1, n2):
     return n1 - n2
 
 
-def multiplication(n1, n2):
+def multiply(n1, n2):
     return n1 * n2
 
 
 def division(n1, n2):
     return n1 / n2
 
+
+operators = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": division
+}
+
+num1 = input('What\'s the number? ')
+operation = input('What\'s the operation? ')
+num2 = input('What\'s the next number?')
 
 
