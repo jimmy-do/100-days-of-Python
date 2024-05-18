256b0bd0f2a212ad1e456fc53136e99163b74756
jimmy-do <59305538+jimmy-do@users.noreply.github.com>
2024-05-17 19:30:06 -0500

    coffee_machine in OOP

--- day16/coffee_machine_OOP/main.py
+++ day16/coffee_machine_OOP/main.py
@@ -2,3 +2,25 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+def play():
+    user_prompt = input('What would you like? (espresso/latte/cappuccino)').lower()
+    if user_prompt == 'report':
+        money_machine.report()
+        coffee_maker.report()
+    if user_prompt == 'off':
+        exit('Please come back soon for more coffee! ️☕️')
+    else:
+        drink = menu.find_drink(user_prompt)
+        enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        if enough_ingredients:
+            money_machine.process_coins()
+        enough_money = money_machine.make_payment(drink.cost)
+        if enough_ingredients and enough_money:
+            coffee_maker.make_coffee(drink)
+
+while True:
+    play()
