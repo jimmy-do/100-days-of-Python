19b02507814b1cd1b55a7cf18de84e0c41094051
jimmy-do <59305538+jimmy-do@users.noreply.github.com>
2024-05-19 13:05:11 -0500

    day 17 commit

--- day17/quiz_proj/main.py
+++ day17/quiz_proj/main.py
@@ -2,3 +2,12 @@ from question_model import Question
 from data import question_data
 
 question_bank = []
+
+for question in question_data:
+    question_text = question['text']
+    question_ans = question['answer']
+    new_question = Question(question_text, question_ans)
+    question_bank.append(new_question)
+    
+print(question_bank)
+
